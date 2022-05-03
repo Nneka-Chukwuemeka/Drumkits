40261ee1cb6d34ecfcecab67c5de035bec409ead
Nneka-Chukwuemeka <nneka.uchechukwu.chukwuemeka@gmail.com>
2022-05-03 18:01:32 +0100

    clicking buttons

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 A drumkit project is a website built with JavaScript, HTML and CSS.
 It is just webpage that you can play different music sound by just pressing some key.
+
+#--------------------------------------------------#
 It is my first JavaScript project in my 30 days of Javascript.
 Pressing A gives you Clap
 Pressing S gives hihat
@@ -11,4 +13,6 @@ Pressing J gives snares
 Pressing K gives tom
 Pressing L gives tink
 
+#------------------------------------------------#
+It is not just the keypad now. you can click on the different buttons and get the sounds. An updated version
 web-address: https://nneka-drumkits.netlify.app/
--- app.js
+++ app.js
@@ -22,3 +22,73 @@ key.addEventListener("click", function (e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   console.log(audio);
 });*/
+
+function clickSound(e) {
+  if (!audio) return;
+
+  audio.play();
+}
+
+const btnA = document.querySelector("#key-a");
+const btnS = document.querySelector("#key-s");
+const btnD = document.querySelector("#key-d");
+const btnF = document.querySelector("#key-f");
+const btnG = document.querySelector("#key-g");
+const btnH = document.querySelector("#key-h");
+const btnJ = document.querySelector("#key-j");
+const btnK = document.querySelector("#key-k");
+const btnL = document.querySelector("#key-l");
+btnA.addEventListener("click", function (e) {
+  const audioA = document.querySelector(".play-A");
+  audioA.currentTime = 0;
+  audioA.play();
+  key.classList.add("playing");
+});
+btnS.addEventListener("click", function (e) {
+  const audioS = document.querySelector(".play-S");
+  audioS.currentTime = 0;
+  audioS.play();
+  key.classList.add("playing");
+});
+btnD.addEventListener("click", function (e) {
+  const audioD = document.querySelector(".play-D");
+  audioD.currentTime = 0;
+  audioD.play();
+  key.classList.add("playing");
+});
+btnF.addEventListener("click", function (e) {
+  const audioF = document.querySelector(".play-F");
+  audioF.currentTime = 0;
+  audioF.play();
+  key.classList.add("playing");
+});
+btnG.addEventListener("click", function (e) {
+  const audioG = document.querySelector(".play-G");
+  audioG.currentTime = 0;
+  audioG.play();
+  key.classList.add("playing");
+});
+btnH.addEventListener("click", function (e) {
+  const audioH = document.querySelector(".play-H");
+  audioH.currentTime = 0;
+  audioH.play();
+  key.classList.add("playing");
+});
+btnJ.addEventListener("click", function (e) {
+  const audioJ = document.querySelector(".play-J");
+  audioJ.currentTime = 0;
+  audioJ.play();
+  key.classList.add("playing");
+});
+btnK.addEventListener("click", function (e) {
+  const audioK = document.querySelector(".play-K");
+  audioK.currentTime = 0;
+  audioK.play();
+  key.classList.add("playing");
+});
+btnL.addEventListener("click", function (e) {
+  const audioL = document.querySelector(".play-L");
+  audioL.currentTime = 0;
+  audioL.play();
+  key.classList.add("playing");
+});
